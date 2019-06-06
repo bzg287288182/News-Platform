094c1ea72fd57c5409943f2e85781a05224c1203
Laobai <287288182@qq.com>
2019-06-06 09:26:43 +0800

    V 0.0.2

--- manage.py
+++ manage.py
@@ -1,11 +1,27 @@
+
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+class Config(object):
+    DEBUG = True
+
+    SQLALCHEMY_DATABASE_URI = "mysql://root:"
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 app = Flask(__name__)
+# 集成配置类
+app.config.from_object(Config)
+app.config.from_pyfile()
+app.config.from_envvar()
+
+db = SQLAlchemy(app)
 
 
-@app.route('/index')
+@app.route('/')
 def index():
-    return 'index'
+    return 'Hello World'
 
 if __name__ == '__main__':
     app.run()
