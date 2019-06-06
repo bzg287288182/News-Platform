ab2e5f0bdc49f08ce8d750fa999d2ea8fc380469
Laobai <287288182@qq.com>
2019-06-06 10:32:22 +0800

    v 0.0.5

---
+++ f
@@ -0,0 +1,23 @@
+[33mcommit e1ae65b516b9ab7ba89f201fde5657d68e614973[m
+Author: Laobai <287288182@qq.com>
+Date:   Thu Jun 6 09:53:51 2019 +0800
+
+    v 0.0.3
+
+[33mcommit 094c1ea72fd57c5409943f2e85781a05224c1203[m
+Author: Laobai <287288182@qq.com>
+Date:   Thu Jun 6 09:26:43 2019 +0800
+
+    V 0.0.2
+
+[33mcommit 880d58fe7af98d864159f6b7ba17ca4769628942[m
+Author: Laobai <287288182@qq.com>
+Date:   Thu Jun 6 09:17:10 2019 +0800
+
+    V 0.0.1
+
+[33mcommit 526a0f49499360e6fe81b7992e37b8d5ba3e6f7c[m
+Author: Laobai <287288182@qq.com>
+Date:   Thu Jun 6 09:15:11 2019 +0800
+
+    V 0.0.1
--- manage.py
+++ manage.py
@@ -1,10 +1,12 @@
+"""
+5.集成flask-session
+"""
 
-
-from flask import Flask
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 from flask_wtf import CSRFProtect
-
+from flask_session import Session
 
 class Config(object):
     DEBUG = True
@@ -16,6 +18,17 @@ class Config(object):
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
 
+
+    # 制定session的储存方式
+    SESSION_TYPE = "redis"
+    #制定储存session的储存对象
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
+    # 设置session签名 加密
+    SESSION_USE_SIGNER = True
+    # 设置session 永久保存
+    SESSION_PERMANENT = False
+    # 设置session保存时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
 
 
 app = Flask(__name__)
@@ -32,7 +45,16 @@ db = SQLAlchemy(app)
 redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 
 # 4.CSRFProtect, 只起到保护的作用，具体往表单和Cookie中设置csrf_token还需要自己做
+# response = make_response("")
+# response.set_cookie("key", "value", max_age=秒数)
 CSRFProtect(app)
+
+# 集成flask-session
+# 说明：flask中的session是保存用户数据的容器（上下文）,而flask_session中的Session是制定session的保存路径
+Session(app)
+
+
+
 
 @app.route('/')
 def index():
