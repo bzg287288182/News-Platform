f922117bed37467b9fb266107dc3dc67d5934a08
Laobai <287288182@qq.com>
2019-06-06 11:24:40 +0800

    抽取配置

---
+++ config.py
@@ -0,0 +1,26 @@
+from redis import StrictRedis
+
+
+
+class Config(object):
+    SECRET_KEY = "12421312"
+    DEBUG = True
+
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/Information"
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    # 给配置类里面自定义了两个类属性
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+
+    # 制定session的储存方式
+    SESSION_TYPE = "redis"
+    #制定储存session的储存对象
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
+    # 设置session签名 加密
+    SESSION_USE_SIGNER = True
+    # 设置session 永久保存
+    SESSION_PERMANENT = False
+    # 设置session保存时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
