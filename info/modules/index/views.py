1fde567f57b89c94aa5d5cdd65edbb2693f587f9
Laobai <287288182@qq.com>
2019-06-08 10:21:01 +0800

    网站图标

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,16 @@
-from info.modules.index import index_blu
+from flask import render_template, redirect, current_app, send_file
 from info import redis_store
+from info.modules.index import index_blu
+
 
 @index_blu.route("/")
 def index():
-    redis_store.set("name","laowang")
-    return "Hello World"
+    # redis_store.set("name","laowang")
+    return render_template("news/index.html")
+
+@index_blu.route("/favicon.ico")
+def favicon():
+    # 返回图片
+    # return redirect("/static/news/favicon.ico")
+    return current_app.send_static_file("news/favicon.ico")
+    # return send_file("/static/news/favicon.ico")
